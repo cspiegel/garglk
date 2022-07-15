1684127c2b77f2f78f0ed82d7cafa51853b0c557
Chris Spiegel <cspiegel@gmail.com>
2022-07-15 08:17:31 -0700

    Use the STL for image caching

--- garglk/garglk.h
+++ garglk/garglk.h
@@ -197,7 +197,6 @@ extern int gli_cellh;
 
 typedef struct rect_s rect_t;
 typedef struct picture_s picture_t;
-typedef struct piclist_s piclist_t;
 typedef struct style_s style_t;
 
 enum FACES { MONOR, MONOB, MONOI, MONOZ, PROPR, PROPB, PROPI, PROPZ };
@@ -217,13 +216,6 @@ struct picture_s
     unsigned char *rgba;
     unsigned long id;
     bool scaled;
-};
-
-struct piclist_s
-{
-    picture_t *picture;
-    picture_t *scaled;
-    struct piclist_s *next;
 };
 
 struct style_s
@@ -796,12 +788,8 @@ picture_t *gli_picture_retrieve(unsigned long id, bool scaled);
 picture_t *gli_picture_scale(picture_t *src, int destwidth, int destheight);
 void gli_picture_increment(picture_t *pic);
 void gli_picture_decrement(picture_t *pic);
-piclist_t *gli_piclist_search(unsigned long id);
-void gli_piclist_clear(void);
 void gli_piclist_increment(void);
 void gli_piclist_decrement(void);
-void gli_picture_store_original(picture_t *pic);
-void gli_picture_store_scaled(picture_t *pic);
 
 window_graphics_t *win_graphics_create(window_t *win);
 void win_graphics_destroy(window_graphics_t *cutwin);
