52c010413ea04fe175e1b5825124ca56443cd795
Chris Spiegel <cspiegel@gmail.com>
2022-08-20 11:49:48 -0700

    Check the right value

--- garglk/zbleep.cpp
+++ garglk/zbleep.cpp
@@ -77,7 +77,7 @@ void Bleeps::update(int number, double duration, int frequency) {
         data.push_back(127 * point);
     }
 
-    if (data.size() % 2 == 1)
+    if (frames % 2 == 1)
         data.push_back(0);
 
     m_bleeps.at(number) = std::move(data);
