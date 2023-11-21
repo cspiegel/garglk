dd6d8c3d6036e1726c0eaae37bcddce22e76dced
Chris Spiegel <cspiegel@gmail.com>
2023-11-21 05:50:01 -0800

    Use local toolchain

--- msvc.sh
+++ msvc.sh
@@ -8,6 +8,6 @@ mkdir build-msvc
 
 (
 cd build-msvc
-cmake .. -DWITH_QT6=ON -DCMAKE_PREFIX_PATH=$HOME/Qt/6.6.0/msvc2019_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/usr/x86_64-pc-windows-msvc/toolchain-x86_64.cmake -DSOUND=QT -DWITH_BUNDLED_FMT=ON
+cmake .. -DWITH_QT6=ON -DCMAKE_PREFIX_PATH=$HOME/Qt/6.6.0/msvc2019_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=Toolchain-msvc.cmake -DSOUND=QT -DWITH_BUNDLED_FMT=ON
 make -j${nproc}
 )
