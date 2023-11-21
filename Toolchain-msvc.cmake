0f6039290f859d1823f0e7c8378472d2478129af
Chris Spiegel <cspiegel@gmail.com>
2023-11-21 05:37:05 -0800

    Update toolchain

--- Toolchain-msvc.cmake
+++ Toolchain-msvc.cmake
@@ -1,9 +1,10 @@
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR X86)
+set(CMAKE_SYSTEM_PROCESSOR X86_64)
 
+set(CMAKE_BUILD_TYPE "Release")
 set(CMAKE_C_COMPILER "x86_64-pc-windows-msvc-gcc")
 set(CMAKE_CXX_COMPILER "x86_64-pc-windows-msvc-g++")
-set(CMAKE_EXE_LINKER_FLAGS "/winsdkdir:/home/chris/Programming/XWin/winsdk/sdk /vctoolsdir:/home/chris/Programming/XWin/winsdk/crt /libpath:/home/chris/Programming/XWin/winsdk/sdk/lib/um/x64")
+set(CMAKE_EXE_LINKER_FLAGS "/winsdkdir:/usr/x86_64-pc-windows-msvc/winsdk/sdk /vctoolsdir:/usr/x86_64-pc-windows-msvc/winsdk/crt /libpath:/usr/x86_64-pc-windows-msvc/winsdk/sdk/lib/um/x64")
 set(CMAKE_SHARED_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS})
 set(CMAKE_MT "llvm-mt")
 
