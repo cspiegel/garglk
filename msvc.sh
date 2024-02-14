8b31a9d51e11c35e1d2b6427a0e5e39aa610ba2d
Chris Spiegel <cspiegel@gmail.com>
2024-02-14 13:33:41 -0800

    Remove unnecessary subshell

--- msvc.sh
+++ msvc.sh
@@ -6,8 +6,6 @@ nproc=$(getconf _NPROCESSORS_ONLN)
 
 mkdir build-msvc
 
-(
 cd build-msvc
 cmake .. -DWITH_QT6=ON -DCMAKE_PREFIX_PATH=$HOME/Qt/6.6.0/msvc2019_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=Toolchain-msvc.cmake -DSOUND=QT -DWITH_BUNDLED_FMT=ON
 make -j${nproc}
-)
