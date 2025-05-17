3e245203e250145692527980638bf0ef2ea89672
Chris Spiegel <cspiegel@gmail.com>
2025-05-17 08:24:56 -0700

    Remove unused headers

--- garglk/style.cpp
+++ garglk/style.cpp
@@ -17,7 +17,6 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <stdexcept>
-#include <variant>
 
 #include "glk.h"
 #include "garglk.h"
--- garglk/wingfx.cpp
+++ garglk/wingfx.cpp
@@ -18,7 +18,6 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <memory>
-#include <variant>
 
 #include "glk.h"
 #include "garglk.h"
